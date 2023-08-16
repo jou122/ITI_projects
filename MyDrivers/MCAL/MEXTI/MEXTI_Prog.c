05aa787bb0371e3ff7b5b5669dcab8096d653d8d
Youssef Mohammed <36369688+jou122@users.noreply.github.com>
2023-08-16 23:40:53 +0300

    edited MEXTI

--- MyDrivers/MCAL/MEXTI/MEXTI_Prog.c
+++ MyDrivers/MCAL/MEXTI/MEXTI_Prog.c
@@ -17,8 +17,9 @@
 
 static void(*EXIT_CallBack_Arr[16])(void);
 
-
-
+void MEXTI_ClearPendingReg(u8 Copy_u8Line ){
+	SET_BIT(MEXTI->PR,Copy_u8Line);
+}
 
 void MEXTI_voidEnableEXTI(u8 Copy_u8Line)
 {
@@ -28,7 +29,17 @@ void MEXTI_voidDisableEXTI(u8 Copy_u8Line)
 {
 	CLR_BIT(MEXTI->IMR , Copy_u8Line);
 }
-void MEXTI_voidSoftware(u8 Copy_u8Line)
+
+
+void MEXTI_voidEventMaskEnable(u8 Copy_u8Line){
+	SET_BIT(MEXTI->EMR , Copy_u8Line);
+}
+void MEXTI_voidEventMaskDisable(u8 Copy_u8Line){
+	CLR_BIT(MEXTI->EMR , Copy_u8Line);
+}
+
+
+void MEXTI_voidSetSoftware(u8 Copy_u8Line)
 {
 	SET_BIT(MEXTI -> SWIER , Copy_u8Line);
 }
@@ -38,20 +49,30 @@ void MEXTI_voidSelectTrigger(u8 Copy_u8Line , u8 Copy_u8Mode)
 {
 	switch(Copy_u8Mode)
 	{
-		case RISING 	:	SET_BIT(MEXTI -> RTSR , Copy_u8Line); 	break;
-		case FALLING	:	SET_BIT(MEXTI -> FTSR , Copy_u8Line); 	break;
+		case RISING 	:	SET_BIT(MEXTI -> RTSR , Copy_u8Line);
+							CLR_BIT(MEXTI -> FTSR , Copy_u8Line); 	break;
+		case FALLING	:	SET_BIT(MEXTI -> FTSR , Copy_u8Line);
+							CLR_BIT(MEXTI -> RTSR , Copy_u8Line) 	break;
 		case ON_CHANGE 	:	SET_BIT(MEXTI -> RTSR , Copy_u8Line);
 							SET_BIT(MEXTI -> FTSR , Copy_u8Line); 	break;
 	}
 }
 
 
-void MEXIT_voidCallback(u8 Copy_u8InNum,void(*ptr)(void))
+
+
+
+void MEXIT_voidSetCallback(u8 Copy_u8InNum,void(*ptr)(void))
 {
 	EXIT_CallBack_Arr[Copy_u8InNum] = ptr;
 }
 
 void EXTI0_IRQHandler(void){
 	EXIT_CallBack_Arr[0]();
-	SET_BIT(MEXTI->PR,0);
+	MEXTI_ClearPendingReg(LINE0);
+}
+
+void EXTI1_IRQHandler(void){
+	EXIT_CallBack_Arr[1]();
+	MEXTI_ClearPendingReg(LINE1);
 }
